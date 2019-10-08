bac97be8132128fb7979e647099c997401ba4ef0
HanMoonHee <mun01180@gmail.com>
2019-10-08 19:29:28 +0900

    update

---
+++ chapter01/src/sample02/HelloSpring.java
@@ -0,0 +1,11 @@
+package sample02;
+
+public class HelloSpring {
+
+	public static void main(String[] args) {
+		MessageBean messageBean;
+		messageBean = new MessageBeanKo();
+		//messageBean = new MessageBeanEn();
+		messageBean.sayHello("Spring");
+	}
+}
---
+++ chapter01/src/sample03/HelloSpring.java
@@ -0,0 +1,25 @@
+package sample03;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class HelloSpring {
+
+	public static void main(String[] args) {
+		//ApplicationContext context = new FileSystemXmlApplicationContext("src/applicationContext.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		MessageBean bean = (MessageBean) context.getBean("messageBean");
+		bean.sayHello("Spring");
+		System.out.println();
+		
+		MessageBean bean2 = (MessageBean) context.getBean("messageBean");
+		bean.sayHello("Spring");
+		System.out.println();
+		
+		MessageBean bean3 = (MessageBean) context.getBean("messageBean");
+		bean.sayHello("Spring");
+		System.out.println();
+	}
+}
